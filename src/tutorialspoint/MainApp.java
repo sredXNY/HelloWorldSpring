c6636d5f1aa4d788223506e10aa3b4ed632134f6
sredny <sredny.nevermind92@gmail.com>
2017-01-28 18:54:02 -0300

    inheritance with javaBean

--- src/tutorialspoint/MainApp.java
+++ src/tutorialspoint/MainApp.java
@@ -10,8 +10,15 @@ public class MainApp {
     public static void main(String[] args){
         AbstractApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
 
-        HelloWorld obj= (HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+        objA.getMessage1();
+        objA.getMessage2();
+
+        HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
+        objB.getMessage1();
+        objB.getMessage2();
+        objB.getMessage3();
         context.registerShutdownHook();
     }
 }
