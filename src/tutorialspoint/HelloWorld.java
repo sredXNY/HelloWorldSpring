4fdb4fffe10334648ebb96c36277257286062aaa
sredny <sredny.nevermind92@gmail.com>
2017-01-28 18:21:56 -0300

    init and destriy functions of the bean

--- src/tutorialspoint/HelloWorld.java
+++ src/tutorialspoint/HelloWorld.java
@@ -1,9 +1,12 @@
 package tutorialspoint;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 /**
  * Created by sredxny on 28-01-2017.
  */
-public class HelloWorld {
+public class HelloWorld implements InitializingBean, DisposableBean{
     private String message;
 
     public void setMessage(String message){
@@ -12,5 +15,16 @@ public class HelloWorld {
 
     public void getMessage(){
         System.out.println(this.message);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //some stuff here!
+        System.out.println("Creating bean");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroying bean");
     }
 }
--- src/tutorialspoint/MainApp.java
+++ src/tutorialspoint/MainApp.java
@@ -1,5 +1,6 @@
 package tutorialspoint;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -7,9 +8,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class MainApp {
     public static void main(String[] args){
-        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
 
         HelloWorld obj= (HelloWorld) context.getBean("helloWorld");
         obj.getMessage();
+        context.registerShutdownHook();
     }
 }
