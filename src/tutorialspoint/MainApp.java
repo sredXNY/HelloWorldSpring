30631f65ec93c8962be3c363d0238f3a24838acd
sredny <sredny.nevermind92@gmail.com>
2017-02-02 10:41:15 -0300

    aspects with xml notation

--- src/tutorialspoint/MainApp.java
+++ src/tutorialspoint/MainApp.java
@@ -9,12 +9,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class MainApp {
     public static void main(String[] args){
-        ConfigurableApplicationContext context =
+        ApplicationContext context =
                 new ClassPathXmlApplicationContext("Beans.xml");
 
-        CustomEventPublisher cvp =
-                (CustomEventPublisher) context.getBean("customEventPublisher");
-        cvp.publish();
-        cvp.publish();
+        Student student = (Student) context.getBean("student");
+
+        student.getName();
+        student.getAge();
+
+        student.printThrowException();
     }
 }
