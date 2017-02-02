15aad746d0ce0788b9d77fa982b21150cc79d2ee
sredny <sredny.nevermind92@gmail.com>
2017-02-02 17:09:19 -0300

    jdbc CRUD

--- src/tutorialspoint/Logging.java
+++ src/tutorialspoint/Logging.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 * For example, a logging module would be called AOP aspect for logging.
 * An application can have any number of aspects depending on the requirement.
 * */
-@Aspect
 public class Logging {
     /** Following is the definition for a pointcut to select
      *  all the methods available. So advice will be called
@@ -20,14 +19,12 @@ public class Logging {
     /*
     * This is a set of one or more joinpoints where an advice should be executed.
     * */
-    @Pointcut("execution(* tutorialspoint.*.*(..))")
     private void selectAll(){}
 
     /**
      * This is the method which I would like to execute
      * before a selected method execution.
      */
-    @Before("selectAll()")
     public void beforeAdvice(){
         System.out.println("Going to setup student profile.");
     }
@@ -36,7 +33,6 @@ public class Logging {
      * This is the method which I would like to execute
      * after a selected method execution.
      */
-    @After("selectAll()")
     public void afterAdvice(){
         System.out.println("Student profile has been setup.");
     }
@@ -45,17 +41,8 @@ public class Logging {
      * This is the method which I would like to execute
      * when any method returns.
      */
-    @AfterReturning(pointcut = "selectAll()", returning="retVal")
     public void afterReturningAdvice(Object retVal){
         System.out.println("Returning:" + retVal.toString() );
     }
 
-    /**
-     * This is the method which I would like to execute
-     * when any method returns.
-     */
-    @AfterReturning(pointcut = "selectAll()", returning="retVal")
-    public void afterReturningAdvice(Object retVal){
-        System.out.println("Returning:" + retVal.toString() );
-    }
 }
--- src/tutorialspoint/Student.java
+++ src/tutorialspoint/Student.java
@@ -6,7 +6,15 @@ package tutorialspoint;
 public class Student {
     private Integer age;
     private String name;
+    private Integer id;
 
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
     public void setAge(Integer age) {
         this.age = age;
     }
